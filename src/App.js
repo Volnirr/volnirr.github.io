7d65244976d48964d9413d84853763a63c780d9d
volnirr <liamgillaspy0@gmail.com>
2025-01-08 14:48:19 -0800

    basic site frame added

--- src/App.js
+++ src/App.js
@@ -4,28 +4,49 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="p-4 bg-gray-100">
-      <ul className="flex gap-4">
-        <li><Link to="/" className="hover:text-blue-600">Home</Link></li>
-        <li><Link to="/about" className="hover:text-blue-600">About</Link></li>
-      </ul>
+        <p>
+          <Link to="/hub" className="hover:text-blue-600">Hub - </Link>
+          <Link to="/projects" className="hover:text-blue-600">Projects - </Link>
+          <Link to="/about" className="hover:text-blue-600">About</Link>
+        </p>
     </nav>
   );
 }
 
 function Home() {
   return (
+    <div className="App-header">
+      <p>Welcome</p>
+      <p> 
+        | <br></br>
+        | <br></br>
+        ˇ 
+      </p>
+      <Link to="/hub"className="Main-button">click</Link>
+    </div>
+  );
+}
+
+function Hub() {
+  return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Welcome to My Site</h1>
-      <p>This is the home page. Start building your content here!</p>
+      <h1 className="text-2xl font-bold mb-4">Hub</h1>
+    </div>
+  );
+}
+
+function Projects() {
+  return(
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Projects</h1>
     </div>
   );
 }
 
 function About() {
-  return (
+  return(
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">About</h1>
-      <p>This is the about page. Add your information here!</p>
     </div>
   );
 }
@@ -33,11 +54,31 @@ function About() {
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Navbar />
+      <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={
+            <div>
+              <Home />
+            </div>
+          } />
+          <Route path="/hub" element={
+            <div>
+              <Navbar />
+              <Hub />
+            </div>
+          } />
+          <Route path="/projects" element={
+            <div>
+              <Navbar />
+              <Projects />
+            </div>
+          } />
+          <Route path="/about" element={
+            <div>
+              <Navbar />
+              <About />
+            </div>
+          } />
         </Routes>
       </div>
     </BrowserRouter>
