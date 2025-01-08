2a7a173d543c80b6c500e86fcf52f0a03e805930
volnirr <liamgillaspy0@gmail.com>
2025-01-08 13:34:49 -0800

    basic page layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="p-4 bg-gray-100">
+      <ul className="flex gap-4">
+        <li><Link to="/" className="hover:text-blue-600">Home</Link></li>
+        <li><Link to="/about" className="hover:text-blue-600">About</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Welcome to My Site</h1>
+      <p>This is the home page. Start building your content here!</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">About</h1>
+      <p>This is the about page. Add your information here!</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello.
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
